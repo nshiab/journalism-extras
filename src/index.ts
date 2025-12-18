7b2e61c81c03fe65ecd527227ef530402dab70a1
nshiab <nael.shiab@gmail.com>
2025-12-18 11:06:22 -0500

    First commit

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+/**
+ * @module
+ *
+ * The Journalism library
+ *
+ * To install the library with Deno, use:
+ * ```bash
+ * deno add jsr:@nshiab/journalism-extras
+ * ```
+ *
+ * To install the library with Node.js, use:
+ * ```bash
+ * npx jsr add @nshiab/journalism-extras
+ * ```
+ *
+ * To import a function, use:
+ * ```ts
+ * import { functionName } from "@nshiab/journalism-extras";
+ * ```
+ */
+
+import unzip from "./extras/unzip.ts";
+import zip from "./extras/zip.ts";
+import createDirectory from "./extras/createDirectory.ts";
+import removeDirectory from "./extras/removeDirectory.ts";
+import getId from "./extras/getId.ts";
+import sleep from "./extras/sleep.ts";
+import DurationTracker from "./extras/DurationTracker.ts";
+
+export {
+  createDirectory,
+  DurationTracker,
+  getId,
+  removeDirectory,
+  sleep,
+  unzip,
+  zip,
+};
---
+++ src/web.ts
@@ -0,0 +1,16 @@
+/**
+ * This module provides a collection of functions to be used in web applications.
+ *
+ * To import a function, use:
+ * ```ts
+ * import { functionName } from "@nshiab/journalism-extras/web";
+ * ```
+ *
+ * @module
+ */
+
+import getId from "./extras/getId.ts";
+import sleep from "./extras/sleep.ts";
+import DurationTracker from "./extras/DurationTracker.ts";
+
+export { DurationTracker, getId, sleep };
